834ea0a3b5846aa635cb4f36166199226b239349
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-10-12 11:32:29 +0200

    docs(game): remove comments

--- SnakeGame/Game.cs
+++ SnakeGame/Game.cs
@@ -2,14 +2,12 @@
 {
     static void Main(string[] args)
     {
-        // Create two players from the console
         Console.WriteLine("Joueur 1, entrez votre nom :");
         string name1 = Console.ReadLine();
 
         Console.WriteLine("Joueur 2, entrez votre nom :");
         string name2 = Console.ReadLine();
 
-        // Create a counter for each player
         int counter1 = 0;
         int counter2 = 0;
 
@@ -17,17 +15,14 @@
 
         while (!finished)
         {
-            // Tell player 1 to roll the dice
             Console.WriteLine(name1 + ", appuyez sur Entrée pour lancer le dé.");
             Console.ReadLine();
 
-            // Generate a random number between 1 and 6
             Random random = new Random();
             int dice1 = random.Next(1, 7);
 
             Console.WriteLine("C'est un " + dice1 + "!");
 
-            // Increase counter for player 2
             counter1 += dice1;
             if (counter1 == 50)
             {
@@ -42,16 +37,13 @@
 
             if(counter1 != 50)
             {
-                // Tell player 2 to roll the dice
                 Console.WriteLine(name2 + ", appuyez sur Entrée pour lancer le dé.");
                 Console.ReadLine();
 
-                // Generate a random number between 1 and 6
                 int dice2 = random.Next(1, 7);
 
                 Console.WriteLine("C'est un " + dice2 + "!");
 
-                // Increase counter for player 2
                 counter2 += dice2;
                 if (counter2 == 50)
                 {
